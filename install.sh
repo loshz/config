78186ce1100bf9fe05f82af617a93724c6892653
Dan Bond <danbondd@gmail.com>
2018-05-28 17:12:18 +0100

    remove files dir

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ FILES["nvim/init.vim"]="$HOME/.config/nvim"
 FILES["nvim/dark.vim"]="$HOME/.config/nvim/colors"
 FILES["zsh/.zshrc"]="$HOME"
 FILES["zsh/terminal.zsh-theme"]="$HOME/.oh-my-zsh/themes"
-FILES[".gitconfig"]="$HOME"
+FILES["git/.gitconfig"]="$HOME"
 
 if [[ "$OSTYPE" == "linux"* ]]; then
-	FILES[".Xresources"]="$HOME"
+	FILES["X/.Xresources"]="$HOME"
+	FILES["X/.xinitrc"]="$HOME"
 	FILES["i3/config"]="$HOME/.config/i3"
 	FILES["i3/i3blocks.conf"]="$HOME/.config/i3"
 fi
@@ -19,10 +20,10 @@ TOTAL=${#FILES[@]}
 for i in "${!FILES[@]}"; do
 	mkdir -p "${FILES[$i]}"
 	FILE="${FILES[$i]}/${i##*/}"
-	ln -sfv $HOME/.dotfiles/files/$i $FILE
+	ln -sfv $HOME/.dotfiles/$i $FILE
 	if [ $? -eq 0 ]; then
 		((COUNT++))
 	fi
 done
 
-echo "successfully linked $COUNT out of $TOTAL files."
+echo -e "\nsuccessfully linked ${COUNT} out of ${TOTAL} files"
